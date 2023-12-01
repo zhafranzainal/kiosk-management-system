b8a50b36b571c0ddf4a872d27691ec52ab77c146
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-01 15:37:52 +0800

    Separate summary view into column

--- resources/views/applications/show.blade.php
+++ resources/views/applications/show.blade.php
@@ -25,70 +25,75 @@
                         </h5>
                     </div>
 
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            @lang('crud.applications.inputs.user_id')
-                        </h5>
-                        <span>{{ optional($application->user)->name ?? '-' }}</span>
+                    <div class="columns-2">
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                @lang('crud.applications.inputs.user_id')
+                            </h5>
+                            <span>{{ optional($application->user)->name ?? '-' }}</span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Course
+                            </h5>
+                            <span>
+                                {{ $application->user?->kioskParticipant?->student?->course?->name ?? '-' }}
+                            </span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Year / Semester
+                            </h5>
+                            <span>
+                                Year {{ $application->user?->kioskParticipant?->student?->year ?? '-' }}
+                                Semester {{ $application->user?->kioskParticipant?->student?->semester ?? '-' }}
+                            </span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Contact Number
+                            </h5>
+                            <span>{{ optional($application->user)->mobile_no ?? '-' }}</span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Kiosk Number
+                            </h5>
+                            <span>FKK{{ str_pad(optional($application->kiosk)->id, 2, '0', STR_PAD_LEFT) }}</span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                @lang('crud.applications.inputs.kiosk_id')
+                            </h5>
+                            <span>{{ optional($application->kiosk)->name ?? '-' }}</span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Business Type
+                            </h5>
+                            <span>{{ optional(optional($application->kiosk)->businessType)->name ?? '-' }}</span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h5 class="font-medium text-gray-700">
+                                Business Period
+                            </h5>
+                            <span>
+                                {{ optional($application->start_date)->format('j F Y') ?? '-' }}
+                                -
+                                {{ optional($application->end_date)->format('j F Y') ?? '-' }}
+                            </span>
+                        </div>
+
                     </div>
 
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Course
-                        </h5>
-                        <span>
-                            {{ $application->user?->kioskParticipant?->student?->course?->name ?? '-' }}
-                        </span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Year / Semester
-                        </h5>
-                        <span>
-                            Year {{ $application->user?->kioskParticipant?->student?->year ?? '-' }}
-                            Semester {{ $application->user?->kioskParticipant?->student?->semester ?? '-' }}
-                        </span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Contact Number
-                        </h5>
-                        <span>{{ optional($application->user)->mobile_no ?? '-' }}</span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Kiosk Number
-                        </h5>
-                        <span>FKK{{ str_pad(optional($application->kiosk)->id, 2, '0', STR_PAD_LEFT) }}</span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            @lang('crud.applications.inputs.kiosk_id')
-                        </h5>
-                        <span>{{ optional($application->kiosk)->name ?? '-' }}</span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Business Type
-                        </h5>
-                        <span>{{ optional(optional($application->kiosk)->businessType)->name ?? '-' }}</span>
-                    </div>
-
-                    <div class="mb-4">
-                        <h5 class="font-medium text-gray-700">
-                            Business Period
-                        </h5>
-                        <span>
-                            {{ optional($application->start_date)->format('j F Y') ?? '-' }}
-                            -
-                            {{ optional($application->end_date)->format('j F Y') ?? '-' }}
-                        </span>
-                    </div>
 
                 </div>
 
