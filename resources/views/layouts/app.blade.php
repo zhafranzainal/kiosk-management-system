5d6fcb94692ad59821766f7675ac8346b0cc26df
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-08 09:45:27 +0800

    Create base layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,86 +14,232 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    <link href="assets/css/vendor/dataTables.bootstrap4.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/vendor/responsive.bootstrap4.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style">
 
     <!-- Icons -->
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
 
+    <!-- Datatables css -->
+    <link href="assets/css/vendor/dataTables.bootstrap4.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/vendor/responsive.bootstrap4.css" rel="stylesheet" type="text/css">
+
 </head>
 
-<body class="font-sans antialiased">
+<body class="loading"
+    data-layout-config='{"leftSideBarTheme":"default","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
 
-    <x-jet-banner />
+    <div class="wrapper">
 
-    <div class="min-h-screen bg-gray-100">
+        <div class="left-side-menu">
 
-        @livewire('navigation-menu')
+            <!-- LOGO -->
+            <a href="index.html" class="logo text-center logo-light">
+                <span class="logo-lg">
+                    <img src="assets/images/logo.png" alt="" height="16">
+                </span>
+                <span class="logo-sm">
+                    <img src="assets/images/logo_sm.png" alt="" height="16">
+                </span>
+            </a>
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+            <div class="h-100" id="left-side-menu-container" data-simplebar>
+
+                <ul class="metismenu side-nav">
+
+                    {{-- Profile and Role --}}
+                    <div class="side-nav-link">
+                        <a href="javascript: void(0);">
+                            <img src="assets/images/users/avatar-1.jpg" alt="user-image" height="35"
+                                class="rounded-circle shadow-sm">
+                            <div class="user-avatar">
+                                <span class="leftbar-user-name text-white" style="font-weight: bold">Ain Syazwani</span>
+                                <p class="text-white font-14">Kiosk Participant</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <li class="side-nav-title side-nav-item">Menu</li>
+
+                    @livewire('navigation-menu')
+
+                </ul>
+
+                <div class="clearfix"></div>
+
+            </div>
+
+        </div>
+
+        <div class="content-page">
+
+            <div class="content">
+
+                {{-- Navigation Bar --}}
+                <div class="navbar-custom" style="background-color: #CA3433;">
+
+                    <ul class="list-unstyled topbar-right-menu float-right mb-0">
+
+                        @if (Auth::user()->can('view-any', Spatie\Permission\Models\Role::class) ||
+                                Auth::user()->can('view-any', Spatie\Permission\Models\Permission::class))
+                            <li class="dropdown notification-list">
+
+                                <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown"
+                                    href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                    <span> Access Management </span>
+                                </a>
+
+                                <div
+                                    class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
+
+                                    @can('view-any', Spatie\Permission\Models\Role::class)
+                                        <a href="{{ route('roles.index') }}" class="dropdown-item notify-item">
+                                            <span>Roles</span>
+                                        </a>
+                                    @endcan
+
+                                    @can('view-any', Spatie\Permission\Models\Permission::class)
+                                        <a href="{{ route('permissions.index') }}" class="dropdown-item notify-item">
+                                            <span>Permissions</span>
+                                        </a>
+                                    @endcan
+
+                                </div>
+
+                            </li>
+                        @endif
+
+                        {{-- Notification --}}
+                        <li class="dropdown notification-list">
+
+                            <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#"
+                                role="button" aria-haspopup="false" aria-expanded="false">
+                                <i class="dripicons-bell noti-icon text-white"></i>
+                                <span class="noti-icon-badge"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
+
+                                <div class="dropdown-item noti-title">
+                                    <h5 class="m-0">
+                                        <span class="float-right">
+                                            <a href="javascript: void(0);" class="text-dark">
+                                                <small>Clear All</small>
+                                            </a>
+                                        </span>
+                                        Notification
+                                    </h5>
+                                </div>
+
+                                <div style="max-height: 230px;" data-simplebar>
+                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                        <div class="notify-icon bg-primary">
+                                            <i class="mdi mdi-comment-account-outline"></i>
+                                        </div>
+                                        <p class="notify-details">Caleb Flakelar commented on Admin
+                                            <small class="text-muted">1 min ago</small>
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <a href="javascript:void(0);"
+                                    class="dropdown-item text-center text-primary notify-item notify-all">
+                                    View All
+                                </a>
+
+                            </div>
+
+                        </li>
+
+                        {{-- Profile --}}
+                        <li class="dropdown notification-list">
+
+                            <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown"
+                                href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                <span class="account-user-avatar">
+                                    <img src="assets/images/users/avatar-1.jpg" alt="user-image"
+                                        class="rounded-circle">
+                                </span>
+                            </a>
+
+                            <div
+                                class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
+
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <i class="mdi mdi-account-circle mr-1"></i>
+                                    <span>My Profile</span>
+                                </a>
+
+                                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <i class="mdi mdi-logout mr-1"></i>
+                                    <span>Logout</span>
+                                </a>
+
+                            </div>
+
+                        </li>
+
+                    </ul>
+
+                    <button class="button-menu-mobile open-left disable-btn text-white">
+                        <i class="mdi mdi-menu"></i>
+                    </button>
+
                 </div>
-            </header>
-        @endif
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+                {{-- Page Content --}}
+                <div class="container-fluid">
+                    {{ $slot }}
+                </div>
+
+            </div>
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> © FKKMS
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
 
     </div>
 
-    @stack('modals')
+    <!-- bundle -->
+    <script src="assets/js/vendor.min.js"></script>
+    <script src="assets/js/app.min.js"></script>
 
-    @livewireScripts
+    <!-- third party js -->
+    <script src="assets/js/vendor/jquery.dataTables.min.js"></script>
+    <script src="assets/js/vendor/dataTables.bootstrap4.js"></script>
+    <script src="assets/js/vendor/dataTables.responsive.min.js"></script>
+    <script src="assets/js/vendor/responsive.bootstrap4.min.js"></script>
+    <script src="assets/js/vendor/dataTables.checkboxes.min.js"></script>
 
-    @stack('scripts')
+    <!-- demo app -->
+    <script src="assets/js/pages/demo.customers.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+    <!-- Datatables js -->
+    <script src="assets/js/vendor/jquery.dataTables.min.js"></script>
+    <script src="assets/js/vendor/dataTables.bootstrap4.js"></script>
+    <script src="assets/js/vendor/dataTables.responsive.min.js"></script>
+    <script src="assets/js/vendor/responsive.bootstrap4.min.js"></script>
 
-    @if (session()->has('success'))
-        <script>
-            var notyf = new Notyf({
-                dismissible: true
-            })
-            notyf.success('{{ session('success') }}')
-        </script>
-    @endif
-
-    <script>
-        /* Simple Alpine Image Viewer */
-        document.addEventListener('alpine:init', () => {
-            Alpine.data('imageViewer', (src = '') => {
-                return {
-                    imageUrl: src,
-
-                    refreshUrl() {
-                        this.imageUrl = this.$el.getAttribute("image-url")
-                    },
-
-                    fileChosen(event) {
-                        this.fileToDataUrl(event, src => this.imageUrl = src)
-                    },
-
-                    fileToDataUrl(event, callback) {
-                        if (!event.target.files.length) return
-
-                        let file = event.target.files[0],
-                            reader = new FileReader()
-
-                        reader.readAsDataURL(file)
-                        reader.onload = e => callback(e.target.result)
-                    },
-                }
-            })
-        })
-    </script>
+    <!-- Datatable Init js -->
+    <script src="assets/js/pages/demo.datatable-init.js"></script>
 
 </body>
 
